d40536f5e7b9b3797cc16b7a398314b430ad5f36
Chase Ladner <chaseladner@Chases-MacBook-Pro.local>
2016-05-10 13:17:32 -0500

    Added index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>IT'S ON FIRE!</title>
+</head>
+<body>
+	<h2>Welcome to Chaseladner.com!</h2>
+	<p>We were having some issues with our hosting set up, which will be resolved soon.</p>
+	<p>Chalk it up to a learning experience.</p>
+	<p>Anycase, come back soon for an actual website.</p>
+</body>
+</html>
